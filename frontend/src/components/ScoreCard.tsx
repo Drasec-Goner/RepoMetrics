569e7b9a050e5ad4f334b132d2c79d0fc22b50d6
Drasec_Goner <drasec.04@gmail.com>
2026-03-26 10:03:00 +0530

    feat: refactor feature extraction

--- frontend/src/components/ScoreCard.tsx
+++ frontend/src/components/ScoreCard.tsx
@@ -21,3 +21,4 @@ const RiskMeter = ({ score }: Props) => {
 };
 
 export default RiskMeter;# refactor: fix API response bug at 2026-03-22 15:38:00
+# feat: refactor feature extraction at 2026-03-26 10:03:00
